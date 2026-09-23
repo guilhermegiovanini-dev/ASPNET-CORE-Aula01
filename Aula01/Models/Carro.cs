d27784a803c6832f5e5183fd00aad0ec656d3fec
unknown <2amod@e041edu.local>
2026-09-23 14:53:18 -0300

    feat: Criando classe Carro

---
+++ Aula01/Models/Carro.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula01.Models
+{
+    public class Carro
+    {
+
+
+        //PROPIEDADES(ATRIBUTOS/ CARACTERISTICAS)
+
+        public string Marca { get; set; }
+
+
+        public string Modelo { get; set; }
+
+        public string Cor {  get; set; }
+
+        //MÉTODOS (AÇÕES)
+
+        public string Acelerar()
+        {
+            return $"o {Modelo}  {Marca}  está acelerando";
+        }
+
+    }
+}
